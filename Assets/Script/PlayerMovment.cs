4cef63b8f32ec42e67e1ce2605d93c62136883ba
fabio98p <parisifabio98@gmail.com>
2023-05-08 22:25:37 +0200

    edit movment

--- Assets/Script/PlayerMovment.cs
+++ Assets/Script/PlayerMovment.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,18 +7,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovment : MonoBehaviour
 {
-    [SerializeField] float movmentVelocity = 100f;
+    [SerializeField] float movmentVelocity = 250f;
 
     Vector2 moveInput;
     Rigidbody2D rb2d;
+    Animator animator;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -29,6 +33,17 @@ public class PlayerMovment : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * movmentVelocity * Time.deltaTime, rb2d.velocity.y);
         rb2d.velocity = playerVelocity;
+
+        bool playerIsRunning = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerIsRunning);
+    }
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x), 1f);
+        }
     }
 
 }
